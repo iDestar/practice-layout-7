7ca6bb2ff86196c4718b2bad3fe55934e897bc38
iDestar <53664560+iDestar@users.noreply.github.com>
2021-06-15 11:14:17 +0300

    sum

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './style.scss';
+
+const showMenu = function (toggleId, navId) {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+  if (toggle) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+
+showMenu('nav-toggle', 'nav-menu');
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function lickAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((el) => el.addEventListener('click', lickAction));
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+let mode = 'development';
+let target = 'web';
+
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+  target = 'browserslist';
+}
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
+  mode: mode,
+  target: target,
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
+      {
+        test: /\.(png|jpg?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
       {
         test: /\.scss$/i,
         use: [
@@ -14,6 +42,9 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    contentBase: './dist',
   },
   devtool: 'source-map',
 };
