24575d0c67666849ffe05a7b6c05bbe88f7fc4da
iDestar <53664560+iDestar@users.noreply.github.com>
2021-06-23 16:39:58 +0300

    finally

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id');
+    const sectionId = current.getAttribute('id');
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
@@ -65,3 +65,46 @@ function scrollTop() {
 }
 
 window.addEventListener('scroll', scrollTop);
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+
+const iconTheme = `bx-sun`;
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(
+  `.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content,.app__data, .app__img, .contact__data, .contact__button, .footer__content`,
+  {
+    interval: 200,
+  }
+);
